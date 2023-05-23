46a9df28a590c72751aae40afcf7c173b658a4f1
smmurshedmanik <smmanik.dev@gmail.com>
2023-05-23 21:02:35 +0600

    System

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; All rights reserved-2023 | Developed by: S.M.Murshed Manik</p>
+    </div>
+        <!--//footer-->
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+error_reporting(0);
+
+include('includes/dbconnection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <title>Men Salon Management System</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i%7cMontserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Style -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js "></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js "></script>
+<![endif]-->
+</head>
+
+<body>
+    <?php include_once('includes/header.php');?>
+    <div class="hero-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <h1 class="hero-title">Men's salon Management System</h1>
+                    <p class="hero-text"><strong>Your Types. Your Style. Your Color.</strong> </p>
+                    <a href="appointment.php" class="btn btn-default">Make an Appointment</a> </div>
+            </div>
+        </div>
+    </div>
+               </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   
+    <?php include_once('includes/footer.php');?>
+    <!-- /.footer-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menumaker.js"></script>
+    <!-- sticky header -->
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/sticky-header.js"></script>
+</body>
+
+</html>
